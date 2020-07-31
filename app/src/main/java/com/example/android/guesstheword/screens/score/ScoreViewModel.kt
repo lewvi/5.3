f9255b02c408fc0cd22fc2a2bc72e07f82855d1b
lewvi <lew_rip@hotmail.com>
2020-07-31 17:07:56 +0700

    Add LiveData to data binding(edit)

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -3,7 +3,6 @@ package com.example.android.guesstheword.screens.score
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import timber.log.Timber
 
 class ScoreViewModel(finalScore: Int) : ViewModel() {
 
